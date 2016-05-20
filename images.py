be345b01f7ccf2cf7a6773dd5f540bdb85ce12b7
jamesmf <jamesmfrick@gmail.com>
2016-05-20 19:41:24 -0400

    update

--- images.py
+++ images.py
@@ -9,47 +9,56 @@ import numpy as np
 import scipy.misc as mi
 from os import listdir
 
+from keras.models import Sequential
+from keras.layers import Convolution2D, ZeroPadding2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten, Merge
+
+from keras.preprocessing.image import img_to_array, ImageDataGenerator
+
 
 def getModel(img_width, img_height, out_size):
-    model = Sequential()
-    model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))   
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    bigModel = Sequential()
+    bigModel.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))   
+    bigModel.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+    bigModel.add(ZeroPadding2D((1, 1)))
+    bigModel.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+    bigModel.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    bigModel.add(Dropout(0.5))
+        
+    bigModel.add(ZeroPadding2D((1, 1)))
+    bigModel.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+    bigModel.add(ZeroPadding2D((1, 1)))
+    bigModel.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+    bigModel.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    bigModel.add(Dropout(0.5))
     
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    bigModel.add(ZeroPadding2D((1, 1)))
+    bigModel.add(Convolution2D(128, 3, 3, activation='relu', name='conv3_1'))
+    bigModel.add(ZeroPadding2D((1, 1)))
+    bigModel.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+    bigModel.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    bigModel.add(ZeroPadding2D((1, 1)))
+    bigModel.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+    bigModel.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    bigModel.add(Flatten())
     
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    windowModel   = Sequential()
+    windowModel.add(Dense(128,input_shape=(2,)))
+    windowModel.add(Activation('relu'))
     
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model   = Sequential()
+    model.add(Merge([bigModel, windowModel],mode='concat', concat_axis=-1))
+    model.add(Dense(3*out_size**2))
+
+    model.compile(optimizer='adadelta',loss='mse')
     
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    return model
+
 
 def getImage(imPath):
-    return mi.imread(imPath)*1./255
+    return mi.imread(imPath)
 
 def getRegion(image,size=64,downSamp=True,downSampFactor=2):
     """returns a random, non-blank region from an image.
@@ -75,24 +84,40 @@ def getRegion(image,size=64,downSamp=True,downSampFactor=2):
         loc = [np.random.rand(),np.random.rand()]
         upperLeft = [int((image.shape[0]-size)*loc[0]), int((image.shape[1]-size)*loc[1])]
         region = image[upperLeft[0]:upperLeft[0]+size,upperLeft[1]:upperLeft[1]+size]
-        print("check blank: ",np.sum(region), thresholdVal)
         if np.sum(region) > thresholdVal:
             isBlank = False
             #mi.imsave("big.jpg",region)
             if downSamp:
                 region = mi.imresize(region,(newSize,newSize),interp='bilinear')
             mi.imsave("small.jpg",region)
-    print(loc)
-    stop=raw_input(imFile)
+    #print(loc)
     
-    return region, loc
+    return region.flatten(), loc
         
 
-imDir     = "images/"
-imFiles   = listdir(imDir)
+imDir       = "images/"
+imFiles     = listdir(imDir)
+numImages   = len(imFiles)
+imSize      = 128
+outsize     = 32
+batchSize   = 16
+numEpochs   = 10
 
-for imFile in imFiles:
-    imPath    = imDir+imFile
-    image     = getImage(imPath)
-    mi.imsave("wholeImage.jpg",image)
-    region,loc= getRegion(image,downSamp=False)
+model       = getModel(imSize,imSize,outsize)
+
+for epoch in range(0,numEpochs):
+    Xtrain  = np.zeros((numImages,3,imSize,imSize))
+    XtrainL = np.zeros((numImages,2))
+    ytrain  = np.zeros((numImages,3*outsize**2))
+    for count,imFile in enumerate(imFiles):
+        imPath  = imDir+imFile
+        image   = getImage(imPath)
+        image   = mi.imresize(image,(imSize,imSize))
+        #mi.imsave("wholeImage.jpg",image)
+        region,loc= getRegion(image,downSamp=True)
+        image   = img_to_array(image)
+        Xtrain[count,:,:,:] = image
+        XtrainL[count,:] = loc
+        ytrain[count,:] = region
+
+    model.fit([Xtrain,XtrainL],ytrain,nb_epoch=20)
